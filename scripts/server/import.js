33dabe3f2868c90b00fd89e947794469d7c947a8
nieny225 <nieny225@gmail.com>
2023-10-17 16:17:07 +0800

    fix broken recipe and capture error

--- scripts/server/import.js
+++ scripts/server/import.js
@@ -3,7 +3,7 @@
  * All rights reserved.
  */
 
-import fs from 'node:fs';
+import fs from 'node:fs/promises';  // Importing the promises API
 import path from 'node:path';
 import { v4 } from 'uuid';
 import { fileURLToPath } from 'url';
@@ -13,83 +13,82 @@ const MONO_COLLECTION_ID = "legacyMonoCollection";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 function addMetaTag(doc, tag) {
-    if (doc.meta.tags.indexOf(tag) === -1) {
-        doc.meta.tags.push(tag)
+    if (!doc.meta.tags.includes(tag)) {  // Simplified using includes
+        doc.meta.tags.push(tag);
     }
 }
 
-
 async function run() {
-
-    const templateDir = path.join(__dirname, '../../templates');
-    const files = fs.readdirSync(templateDir);  
-
-    const jsonFiles = files.filter(file => 
-        path.extname(file).toLowerCase() === '.json'
-    );    
-
-    const demoRecipes = [
-        ['Bugbear', 'c0deba5e-417d-49df-96d3-8aeb8fc15402'],
-        ['Celestia', 'c0deba5e-786a-4d4b-88d9-1694ebc85527'],
-        ['Chef', 'c0deba5e-7634-49ac-a2b7-878f8476057b'],
-    ]
-
-    let jsonArray = [];
-    for(let file of jsonFiles) {
-        let content = fs.readFileSync(path.join(templateDir, file));
-        let doc = JSON.parse(content);
-        doc.owner = '-----public-----';
-
-        for(let i=0; i<demoRecipes.length; ++i) {
-            const fileNamePrefix = demoRecipes[i][0]
-            const overrideId = demoRecipes[i][1]
-            if(file.startsWith(fileNamePrefix)) {
-                doc.id = overrideId
-                addMetaTag(doc, 'system')
-            }
+    try {
+        const templateDir = path.join(__dirname, '../../templates');
+        if (!(await fs.stat(templateDir)).isDirectory()) {  // Check if directory exists
+            console.error('Directory does not exist:', templateDir);
+            return;
         }
 
-        addMetaTag(doc, 'template');
+        const files = await fs.readdir(templateDir);
+        const jsonFiles = files.filter(file => path.extname(file).toLowerCase() === '.json');
+        const demoRecipes = [
+            ['Bugbear', 'c0deba5e-417d-49df-96d3-8aeb8fc15402'],
+            ['Celestia', 'c0deba5e-786a-4d4b-88d9-1694ebc85527'],
+            ['Chef', 'c0deba5e-7634-49ac-a2b7-878f8476057b'],
+        ];
 
-        doc.meta.org = 'omnitool_core_recipes'
+        let jsonArray = [];
+        for (let file of jsonFiles) {
+            console.log('Processing recipe json:', file);
+            let content = await fs.readFile(path.join(templateDir, file));
+            let doc = JSON.parse(content);
+            doc.owner = '-----public-----';
 
-        doc['publishedTo'] = [];
-        doc['_id'] = `wf:${doc.id}`;
-        jsonArray.push(doc);
+            for (let [fileNamePrefix, overrideId] of demoRecipes) {
+                if (file.startsWith(fileNamePrefix)) {
+                    doc.id = overrideId;
+                    addMetaTag(doc, 'system');
+                }
+            }
+
+            addMetaTag(doc, 'template');
+            doc.meta.org = 'omnitool_core_recipes';
+            doc['publishedTo'] = [];
+            doc['_id'] = `wf:${doc.id}`;
+            jsonArray.push(doc);
+        }
+
+        await reconcilePublishedRecipes(jsonArray);
+    } catch (error) {
+        console.error(error);
     }
-
-    await reconcilePublishedRecipes(jsonArray);
 }
 
 async function reconcilePublishedRecipes(publishedRecipes) {
-    const Pocketbase = (await import('pocketbase')).default;
-    let pb = new Pocketbase('http://127.0.0.1:8090');
-    pb.autoCancellation(false);
+    try {
+        const Pocketbase = (await import('pocketbase')).default;
+        let pb = new Pocketbase(process.env.DATABASE_URL || 'http://127.0.0.1:8090');
+        pb.autoCancellation(false);
 
-    let oldrecords = await pb.collection(MONO_COLLECTION_ID).getFullList(
-        {filter:`omni_id~"wf" && blob.owner="-----public-----"`}
-    );
-    let deleteCmd = [];
-    for(let i=0; i<oldrecords.length; ++i) {
-        deleteCmd.push(pb.collection(MONO_COLLECTION_ID).delete(oldrecords[i].id));
+        let oldrecords = await pb.collection(MONO_COLLECTION_ID).getFullList(
+            { filter: `omni_id~"wf" && blob.owner="-----public-----"` }
+        );
+        let deleteCmd = oldrecords.map(record => pb.collection(MONO_COLLECTION_ID).delete(record.id));
+        await Promise.all(deleteCmd);
+        console.log(`Deleted ${oldrecords.length} demo recipes.`);
+
+        let createCmd = publishedRecipes.map(element => {
+            if (!element.meta.tags.includes("system")) {
+                element.id = v4();
+                element._id = `wf:${element.id}`;
+            }
+            let omni_id = element._id;
+            return pb.collection(MONO_COLLECTION_ID).create({ omni_id: omni_id, blob: element });
+        });
+
+        await Promise.all(createCmd);
+        console.info(`Updated ${createCmd.length} demo recipes.`);
+    } catch (error) {
+        console.error(error);
     }
-    await Promise.all(deleteCmd);
-    console.log(`Deleted ${oldrecords.length} demo recipes.`);
-    let createCmd = [];
-    publishedRecipes.forEach(element => {
-        if (!element.meta.tags.includes("system")) {
-            element.id = v4();
-            element._id = `wf:${element.id}`;
-        }
-        let omni_id = element._id;
-        createCmd.push(pb.collection(MONO_COLLECTION_ID).create(
-            {omni_id: omni_id, blob: element}
-        ));
-    });                
-    await Promise.all(createCmd);
-    console.info(`Updated ${createCmd.length} demo recipes.`);
 }
 
 const script = {
@@ -101,4 +100,5 @@ const script = {
         return { status: 'success', message: 'Recipe import completed.' };
     }
 };
+
 export { run, script };
